359281f59ec760c5048744b248774a32b754ca57
kheber <kerim@heber.org>
2024-08-07 21:28:43 -0400

    fix some typos and correct the headers of the provided genome files

--- make_e_lenta_reads.sh
+++ make_e_lenta_reads.sh
@@ -3,7 +3,7 @@
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/exponential_coverage.txt \
 --n_reads 20000000 \
@@ -12,7 +12,7 @@ iss generate \
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/zero_inflated_lognormal_coverage.txt \
 --n_reads 20000000 \
@@ -21,7 +21,7 @@ iss generate \
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/uniform_coverage.txt \
 --n_reads 20000000 \
@@ -30,7 +30,7 @@ iss generate \
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/lognormal_coverage.txt \
 --n_reads 20000000 \
@@ -39,7 +39,7 @@ iss generate \
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/log_step_coverage.txt \
 --output log_step_e_lenta
@@ -47,7 +47,7 @@ iss generate \
 iss generate \
 --cpus 20 \
 --seed 29032019 \
---genomes genomes_e_lenta/*.fasta \
+--genomes genomes_e_lenta/*.fna \
 --model error_profile.npz \
 --coverage_file coverages_e_lenta/missing_coverage.txt \
 --output missing_e_lenta
@@ -57,7 +57,7 @@ iss generate \
 for i in {1..10}; do
   iss generate \
     --cpus 20 \
-    --genomes genomes_e_lenta/*.fasta \
+    --genomes genomes_e_lenta/*.fna \
     --model error_profile.npz \
     --coverage_file coverages_e_lenta/zil_"$i"_coverage.txt \
     --n_reads 20000000 \
